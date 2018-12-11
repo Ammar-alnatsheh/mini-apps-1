8993899a01fa479e5e70c0f9023215c6209f5459
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 14:11:00 -0800

    simple client page/ tested

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+    var button = $('#generate');
+    button.click(function(event){
+
+        var source = $('#source').val();
+        $('#result').empty().append(source);
+
+    });
+
    
 });
