93ffcc4d77c6accd07ea3a1ce38f7296818e34c8
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 14:17:03 -0800

    implement simple server and tested

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client')));
 
+app.post('/', (req, res) => {
+    res.send('server got your req');
+});
+
+ 
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
