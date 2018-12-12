f21aa8d28310ba7a19ca01690c7e76da8426ab60
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 20:34:25 -0800

    correct the Ajax API for the server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -8,16 +8,17 @@ $(document).ready(function(){
     button.click(function(event){
 
         var source = $('#source').val();
+        var data = JSON.parse(JSON.stringify(source));
 
         $.ajax({
             type: 'POST',
-            data: source,
+            data: data,
             contentType: 'application/json',
             success: function (data) {
                 $('#result').empty().append(data);
             },
-            error: function (data) {
-              alert('server: Failed to build CSV file', data);
+            error: function (error) {
+              alert('server: Failed to build CSV file', error);
             }
           });
 
