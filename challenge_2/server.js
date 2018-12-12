9495c8a3967eacf1dfd5b9bc68188dfac972f8f9
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 20:33:50 -0800

    finish the server code

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser');
 const CSV = require('./CsvGenerator');
 const PORT = 3000;
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 
 app.post('/', (req, res) => {
+    console.log("first step",req.body);
     CSV.generate(req.body, data => {
+        console.log("second step",data);
             res.status(200);
             res.send(data);
     });
