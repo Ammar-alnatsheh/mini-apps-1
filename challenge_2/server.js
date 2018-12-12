885bed393d800a6d1fc95f3400c7ac489989cb78
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 20:40:53 -0800

    clean the code

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -9,9 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 
 app.post('/', (req, res) => {
-    console.log("first step",req.body);
     CSV.generate(req.body, data => {
-        console.log("second step",data);
             res.status(200);
             res.send(data);
     });
