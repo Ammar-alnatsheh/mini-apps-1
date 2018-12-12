3945e3b4bd95715140c564b7c5170ccceedf0b54
Ammar <ammar.alnatsheh@gmail.com>
2018-12-11 17:21:22 -0800

    convert client to Ajax req

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,4 +1,28 @@
-// document.getElementById("generate-form").addEventListener("click", function(event){
-//     event.preventDefault();
-// });
+document.getElementById("generate-form").addEventListener("click", function(event){
+    event.preventDefault();
+});
+
+$(document).ready(function(){
+
+    var button = $('#generate');
+    button.click(function(event){
+
+        var source = $('#source').val();
+
+        $.ajax({
+            type: 'POST',
+            data: source,
+            contentType: 'application/json',
+            success: function (data) {
+                $('#result').empty().append(data);
+            },
+            error: function (data) {
+              alert('server: Failed to build CSV file', data);
+            }
+          });
+
+    });
+
+   
+});
 
