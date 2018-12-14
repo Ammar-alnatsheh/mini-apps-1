63bc1ed42356098fe45d9a596cb767f6ba3c1577
Ammar-alnatsheh <ammar.alnatsheh@gmail.com>
2018-12-13 19:34:25 -0800

    finish the reset function, start implementing the game logic

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -38,7 +38,47 @@ emptyBoard() {
 }
 
 setCell(event) {
-  console.log(event.target.className);
+  var x = event.target.className.split(',')[0];
+  var y = event.target.className.split(',')[1];
+  x = parseInt(x); 
+  y = parseInt(y); 
+
+  var grid = this.state.board;
+  var turn = this.state.turn;
+
+  grid[x][y] = turn;
+
+  if (turn === 1) {
+    turn = 2;
+  } else {
+    turn = 1;
+  }
+
+  if ( this.isGameFinish(grid) ) {
+    var winner = '';
+
+    if (this.state.turn === 1) {
+      winner = this.state.player1;
+    } else {
+      winner = this.state.player1;
+    }
+    alert("Yay Game finished " + winner);
+    this.reset();
+  }
+
+}
+
+isGameFinish(grid) {
+  return false;
+}
+
+reset() {
+  this.setState({
+      board: this.emptyBoard(),
+      turn: 1,
+      player1: this.state.player1,
+      player2: this.state.player2
+  });
 }
 
 render() {
